611e112357aab7b1ab00d76dad516f952abe8dc5
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-02-02 18:47:22 -0300

    Filtros Avançados em APIs com Specification e Paginação

--- src/main/java/com/ead/course/configs/ResolverConfig.java
+++ src/main/java/com/ead/course/configs/ResolverConfig.java
@@ -1,8 +1,14 @@
 package com.ead.course.configs;
 
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class ResolverConfig implements WebMvcConfigurer {
@@ -12,4 +18,14 @@ public class ResolverConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").maxAge(3600);   //permitir que todas as origins tenhan acesso
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+        argumentResolvers.add(new SpecificationArgumentResolver());
+        var pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setFallbackPageable(PageRequest.of(0,2));
+        argumentResolvers.add(pageableResolver);
+    }
+
+
 }
