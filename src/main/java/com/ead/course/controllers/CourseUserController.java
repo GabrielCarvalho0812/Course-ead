f3962e358b64cccb6088fb1768a9651666a1909a
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-03-17 14:28:00 -0300

    Comunicação Síncrona - Inscrição de User em Course - Parte 1

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -1,26 +1,35 @@
 package com.ead.course.controllers;
 
 import com.ead.course.clients.AuthUserClient;
+import com.ead.course.dtos.SubscriptionRecordDto;
 import com.ead.course.dtos.UserRecordDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.CourseUserService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 public class CourseUserController {
 
     private final AuthUserClient authUserClient;
+    private final CourseService courseService;
+    private final CourseUserService courseUserService;
 
-    public CourseUserController(AuthUserClient authUserClient) {
+    public CourseUserController(AuthUserClient authUserClient, CourseService courseService, CourseUserService courseUserService) {
         this.authUserClient = authUserClient;
+        this.courseService = courseService;
+        this.courseUserService = courseUserService;
     }
 
     // traga todos os usuarios relacionados a um determinado course
@@ -28,7 +37,18 @@ public class CourseUserController {
     public ResponseEntity<Page<UserRecordDto>> getAllUserByCourse(@PageableDefault(sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
                                                                    @PathVariable(value = "courseId") UUID courseId){
         return ResponseEntity.status(HttpStatus.OK).body(authUserClient.getAllUsersByCourse(courseId, pageable));
+    }
 
+    @PostMapping("/courses/{courseId}/users/subscription")
+    public ResponseEntity<Object> saveSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId,
+                                                               @RequestBody @Valid SubscriptionRecordDto subscriptionRecordDto){
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if(courseUserService.existsByCourseAndUserId(courseModelOptional.get(), subscriptionRecordDto.userId())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Subscription already exists");
+        }
 
+        CourseUserModel courseUserModel =
+                courseUserService.saveAndSendSubscriptionUserInCourse(courseModelOptional.get().convertToCourseUserModel(subscriptionRecordDto.userId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseUserModel);
     }
 }
--- src/main/java/com/ead/course/models/CourseUserModel.java
+++ src/main/java/com/ead/course/models/CourseUserModel.java
@@ -12,6 +12,12 @@ import java.util.UUID;
 public class CourseUserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    public CourseUserModel(UUID id, UUID userId, CourseModel course) {
+        this.id = id;
+        this.userId = userId;
+        this.course = course;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
@@ -21,6 +27,10 @@ public class CourseUserModel implements Serializable {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private CourseModel course;
+
+    public CourseUserModel() {
+
+    }
 
 
     public UUID getId() {
