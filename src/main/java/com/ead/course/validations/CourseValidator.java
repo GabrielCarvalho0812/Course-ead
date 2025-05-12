c19c711657006cd7ff3d41f7e8dae759b396245e
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-05-12 18:59:27 -0300

    Fluxo Híbrido de Comunicação Síncrona e Assíncrona

--- src/main/java/com/ead/course/validations/CourseValidator.java
+++ src/main/java/com/ead/course/validations/CourseValidator.java
@@ -1,7 +1,10 @@
 package com.ead.course.validations;
 
 import com.ead.course.dtos.CourseRecordDto;
+import com.ead.course.enums.UserType;
+import com.ead.course.models.UserModel;
 import com.ead.course.services.CourseService;
+import com.ead.course.services.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -18,11 +22,12 @@ public class CourseValidator implements Validator {
 
     private final Validator validator;
     final CourseService courseService;
+    final UserService userService;
 
-    public CourseValidator(@Qualifier("defaultValidator") Validator validator, CourseService courseService) {
+    public CourseValidator(@Qualifier("defaultValidator") Validator validator, CourseService courseService, UserService userService) {
         this.validator = validator;
         this.courseService = courseService;
-
+        this.userService = userService;
     }
 
 
@@ -42,6 +47,7 @@ public class CourseValidator implements Validator {
         }
     }
 
+    //metodos privados
     private void validateCourseName(CourseRecordDto courseRecordDto, Errors errors){
         if(courseService.existsByName(courseRecordDto.name())){
             errors.rejectValue("name", "courseNameConflict", "Course Name is Already Taken.");
@@ -50,13 +56,11 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors){
-//        ResponseEntity<UserRecordDto> responseUserInstructor = authUserClient.getOneUserById(userInstructor);
-//        if(responseUserInstructor.getBody().userType().equals(UserType.STUDENT) ||
-//                responseUserInstructor.getBody().userType().equals(UserType.USER)){
-//            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
-//            logger.error("Error validation userInstructor: {} ", userInstructor);
-//    }
+        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+        if(userModelOptional.get().getUserType().equals(UserType.STUDENT.toString()) ||
+                userModelOptional.get().getUserType().equals(UserType.USER.toString())){
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+                logger.error("Error validation userInstructor: {} ", userInstructor);
+        }
     }
-
-
 }
